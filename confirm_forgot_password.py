950f23cfc696004c46fc9c3f5c04b4d845e0b191
Marcel Heinrich <marcellorenzo@gmail.com>
2020-12-18 10:52:19 -0300

    Subindo os arquivos pro GIT

---
+++ confirm_forgot_password.py
@@ -0,0 +1,58 @@
+import json
+import boto3
+import botocore.exceptions
+import hmac
+import hashlib
+import base64
+import uuid
+USER_POOL_ID = '<your user pool id>'
+CLIENT_ID = '<your client id>'
+CLIENT_SECRET ='<your client secret>'
+
+def get_secret_hash(username):
+    msg = username + CLIENT_ID
+    dig = hmac.new(str(CLIENT_SECRET).encode('utf-8'), 
+        msg = str(msg).encode('utf-8'),   digestmod=hashlib.sha256).digest()
+    d2 = base64.b64encode(dig).decode()
+    return d2
+def lambda_handler(event, context):
+    client = boto3.client('cognito-idp')
+    try:
+        username = event['username']
+        password = event['password']
+        code = event['code']
+        client.confirm_forgot_password(
+            ClientId=CLIENT_ID,
+            SecretHash=get_secret_hash(username),
+            Username=username,
+            ConfirmationCode=code,
+            Password=password,
+           )
+    except client.exceptions.UserNotFoundException as e:
+        return {"error": True, 
+                "success": False,
+                "data":  None,
+                "message": "Username doesnt exists"}
+        
+except client.exceptions.CodeMismatchException as e:
+        return {"error": True, 
+               "success": False,
+               "data": None,
+               "message": "Invalid Verification code"}
+        
+    except client.exceptions.NotAuthorizedException as e:
+        return {"error": True, 
+                 "success": False, 
+                 "data": None, 
+                 "message": "User is already confirmed"}
+    
+    except Exception as e:
+        return {"error": True, 
+                "success": False,
+                "data": None,
+                "message": f"Unknown error {e.__str__()} "}
+      
+    return {"error": False, 
+            "success": True, 
+            "message": f"Password has been changed successfully",
+            "data": None}
